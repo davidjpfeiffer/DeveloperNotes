b249756ba51c191ee47ab2d55d15c462014d4e33
David Pfeiffer <djpfeiffer1s@semo.edu>
2016-01-20 21:40:51 -0600

    Added textboxio
    Added textboxio textbox editor to the site for editing and creating
    notes. Optimizations for the added features are still needed.

---
+++ src/DeveloperNotes/wwwroot/lib/textboxio/initialize.js
@@ -0,0 +1,49 @@
+﻿var customToolbar = {
+    items: [
+        {
+            label: 'Group',
+            items: [
+                {
+                    id: 'insert',
+                    label: 'Insert',
+                    items: ['link', 'table']
+                }
+            ]
+        },
+        'style',
+        'emphasis',
+        'align',
+        {
+            label: 'Group',
+            items: ['undo', 'redo']
+        },
+        {
+            label: 'Group',
+            items: ['removeformat', 'fullscreen']
+        }
+    ]
+};
+
+var instantiateTextbox = function () {
+    textboxio.replace('textarea', {
+        css: {
+            styles: [
+                { rule: 'p' },
+                { rule: 'h3', text: 'Heading' },
+            ],
+            stylesheets: ['/lib/textboxio/resources/css/styles.css']
+        },
+        paste: {
+            style: 'clean'
+        },
+        spelling: {}, //http://spelling.ephox.com/
+        ui: { toolbar: customToolbar }
+    })};
+
+instantiateTextbox();
+
+var getEditorContent = function () {
+    var editors = textboxio.get('#textbox');
+    var editor = editors[0];
+    return editor.content.get();
+};
